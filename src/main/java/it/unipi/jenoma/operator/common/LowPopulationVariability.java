c3ed725ffd6a0b83ed0e9adfc1a90b68a1195c15
Diego Casu <d.casu3@studenti.unipi.it>
2021-07-13 20:29:48 +0200

    Added the low population variability termination condition

--- src/main/java/it/unipi/jenoma/operator/common/LowPopulationVariability.java
+++ src/main/java/it/unipi/jenoma/operator/common/LowPopulationVariability.java
@@ -2,21 +2,80 @@ package it.unipi.jenoma.operator.common;
 
 import it.unipi.jenoma.cluster.ClusterLogger;
 import it.unipi.jenoma.operator.TerminationCondition;
+import it.unipi.jenoma.population.Individual;
 import it.unipi.jenoma.population.Population;
 
-import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 
-// TODO: implement
-public class LowPopulationVariability<T extends Serializable> implements TerminationCondition<T> {
-    @Override
-    public T map(Population population, int generationsElapsed, ClusterLogger logger) {
-        return null;
+/**
+ * Termination condition that stops the algorithm when the average fitness of the population
+ * composing the current generation is greater than or equal to a given percentage
+ * of the fitness of its best individual.
+ */
+public class LowPopulationVariability implements TerminationCondition<HashMap<String, Object>> {
+    private final double percentage;
+
+
+    /**
+     * Creates a new <code>LowPopulationVariability</code> operator.
+     * @param percentage  the percentage used to compute the fraction of the
+     *                    best individual's fitness. It must be between 0 and 1.
+     * @throws IllegalArgumentException  if the percentage is not between 0 and 1.
+     */
+    public LowPopulationVariability(double percentage) {
+        if (percentage < 0 || percentage > 1)
+            throw new IllegalArgumentException("The threshold must be a number between 0 and 1.");
+
+        this.percentage = percentage;
     }
 
     @Override
-    public boolean end(List<T> partialConditions, ClusterLogger logger) {
-        return false;
+    public HashMap<String, Object> map(Population population, int generationsElapsed, ClusterLogger logger) {
+        Individual bestIndividual = null;
+        double fitnessAccumulator = 0.0;
+
+        for (Individual individual : population) {
+            fitnessAccumulator += individual.getFitness();
+
+            if (bestIndividual == null)
+                bestIndividual = individual;
+            else if (bestIndividual.getFitness() < individual.getFitness())
+                bestIndividual = individual;
+        }
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("fitnessAccumulator", fitnessAccumulator);
+        result.put("populationLength", population.getLength());
+        result.put("bestIndividual", bestIndividual);
+
+        return result;
+    }
+
+    @Override
+    public boolean end(List<HashMap<String, Object>> partialConditions, ClusterLogger logger) {
+        Individual bestIndividual = null;
+        double totalFitness = 0.0;
+        int totalPopulationLength = 0;
+
+        for (HashMap<String, Object> partialCondition : partialConditions) {
+            totalFitness += (Double) partialCondition.get("fitnessAccumulator");
+            totalPopulationLength += (Integer) partialCondition.get("populationLength");
+            Individual individual = (Individual) partialCondition.get("bestIndividual");
+
+            if (bestIndividual == null)
+                bestIndividual = individual;
+            else if (bestIndividual.getFitness() < individual.getFitness())
+                bestIndividual = individual;
+        }
+
+        double averageFitness = totalFitness/totalPopulationLength;
+        logger.log(String.format(
+                "Average fitness: %s. Best individual fitness: %s.",
+                averageFitness,
+                bestIndividual.getFitness()));
+
+        return averageFitness >= percentage*bestIndividual.getFitness();
     }
 }
